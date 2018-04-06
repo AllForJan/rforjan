5bd7c0c4dd9da5d376018d14329a9965515d5942
Jan Stastny <honza@stastnarodina.com>
2018-04-06 21:25:11 +0200

    Insert even last batch

--- data-import/apa_ziadosti_o_projektove_podpory.rb
+++ data-import/apa_ziadosti_o_projektove_podpory.rb
@@ -2,12 +2,25 @@ require 'hippie_csv'
 require 'awesome_print'
 require_relative 'dbconn'
 
-conn = db_connection
+$conn = db_connection
 
 BATCH_SIZE = 1000
-PREPARED_STATEMENT_NAME = 'insert_apa_ziadosti-o-projektove-podpory_2018-04-03'.freeze
 
 all_values = []
+
+def insert_batch(all_values)
+  placeholders = all_values.each_with_index.map { |batch, idx|
+    dollars = 1.upto(batch.size).map { |i| "$#{i + batch.size * idx}" }.join(',')
+    "(#{dollars})"
+  }.join(', ')
+  sql = "INSERT INTO  apa_ziadosti_projektove_podpory
+(ziadatel, ico, kod_projektu, nazov_projektu, vuc, cislo_vyzvy,
+    kod_podopatrenia, status, datum_zastavenia_konania,
+    dovod_zastavenie_konania, datum_ucinnosti_zmluvy, schvaleny_nfp_celkom,
+    vyplateny_nfp_celkom, pocet_bodov) VALUES #{placeholders}"
+  $conn.exec_params(sql, all_values.flatten)
+end
+
 HippieCSV.read('data/apa_ziadosti-o-projektove-podpory_2018-04-03.csv').each_with_index do |row, index|
   if index == 0
     $header = row
@@ -31,22 +44,15 @@ HippieCSV.read('data/apa_ziadosti-o-projektove-podpory_2018-04-03.csv').each_wit
     all_values << values
 
     if index % BATCH_SIZE == 0
-      placeholders = all_values.each_with_index.map { |batch, idx|
-        dollars = 1.upto(batch.size).map { |i| "$#{i + batch.size * idx}" }.join(',')
-        "(#{dollars})"
-      }.join(', ')
-      sql = "INSERT INTO  apa_ziadosti_projektove_podpory
-(ziadatel, ico, kod_projektu, nazov_projektu, vuc, cislo_vyzvy,
-    kod_podopatrenia, status, datum_zastavenia_konania,
-    dovod_zastavenie_konania, datum_ucinnosti_zmluvy, schvaleny_nfp_celkom,
-    vyplateny_nfp_celkom, pocet_bodov) VALUES #{placeholders}"
-      conn.exec_params(sql, all_values.flatten)
+      insert_batch(all_values)
       all_values = []
-
       ap "Inserted #{index} records"
     end
   end
 end
+
+insert_batch(all_values)
+
 
 
 
