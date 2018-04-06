1cd4d10e629cc0840d59966e5edc93f881fe599a
Jan Stastny <honza@stastnarodina.com>
2018-04-06 21:01:23 +0200

    :lipstick: formatting

--- data-import/apa_ziadosti_o_projektove_podpory.rb
+++ data-import/apa_ziadosti_o_projektove_podpory.rb
@@ -9,19 +9,19 @@ HippieCSV.read('data/apa_ziadosti-o-projektove-podpory_2018-04-03.csv').each_wit
     $header = row
   else
     values = $header.zip(row).map do |field, value|
-        case field
+      case field
         when 'Datum RoN/datum zastavenia konania'
         when 'Datum ucinnosti zmluvy'
           begin
             Date.parse(value)
           rescue
-              nil
+            nil
           end
-          when 'Schvaleny NFP celkom', 'Vyplateny NFP celkom'
-            value.sub(',', '.')
-        else value
-        end
-
+        when 'Schvaleny NFP celkom', 'Vyplateny NFP celkom'
+          value.sub(',', '.')
+        else
+          value
+      end
     end
 
     sql = "
@@ -29,9 +29,10 @@ HippieCSV.read('data/apa_ziadosti-o-projektove-podpory_2018-04-03.csv').each_wit
           (ziadatel, ico, kod_projektu, nazov_projektu, vuc, cislo_vyzvy,
             kod_podopatrenia, status, datum_zastavenia_konania,
             dovod_zastavenie_konania, datum_ucinnosti_zmluvy, schvaleny_nfp_celkom,
-            vyplateny_nfp_celkom, pocet_bodov) VALUES (#{1.upto(values.size).map { |i| "$#{i}"}.join(',') })
+            vyplateny_nfp_celkom, pocet_bodov) VALUES (#{1.upto(values.size).map { |i| "$#{i}" }.join(',') })
     "
     conn.exec_params(sql, values)
+
     if index % 100 == 0
       ap "Imported #{index} records"
     end
