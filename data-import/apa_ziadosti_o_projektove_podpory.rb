1f1f4960c3f12befd63e30d1aa3c6ffc5526d975
Jan Stastny <honza@stastnarodina.com>
2018-04-06 21:21:44 +0200

    Insert in batches

--- data-import/apa_ziadosti_o_projektove_podpory.rb
+++ data-import/apa_ziadosti_o_projektove_podpory.rb
@@ -4,6 +4,10 @@ require_relative 'dbconn'
 
 conn = db_connection
 
+BATCH_SIZE = 1000
+PREPARED_STATEMENT_NAME = 'insert_apa_ziadosti-o-projektove-podpory_2018-04-03'.freeze
+
+all_values = []
 HippieCSV.read('data/apa_ziadosti-o-projektove-podpory_2018-04-03.csv').each_with_index do |row, index|
   if index == 0
     $header = row
@@ -24,17 +28,25 @@ HippieCSV.read('data/apa_ziadosti-o-projektove-podpory_2018-04-03.csv').each_wit
       end
     end
 
-    sql = "
-        INSERT INTO  apa_ziadosti_projektove_podpory
-          (ziadatel, ico, kod_projektu, nazov_projektu, vuc, cislo_vyzvy,
-            kod_podopatrenia, status, datum_zastavenia_konania,
-            dovod_zastavenie_konania, datum_ucinnosti_zmluvy, schvaleny_nfp_celkom,
-            vyplateny_nfp_celkom, pocet_bodov) VALUES (#{1.upto(values.size).map { |i| "$#{i}" }.join(',') })
-    "
-    conn.exec_params(sql, values)
+    all_values << values
 
-    if index % 100 == 0
-      ap "Imported #{index} records"
+    if index % BATCH_SIZE == 0
+      placeholders = all_values.each_with_index.map { |batch, idx|
+        dollars = 1.upto(batch.size).map { |i| "$#{i + batch.size * idx}" }.join(',')
+        "(#{dollars})"
+      }.join(', ')
+      sql = "INSERT INTO  apa_ziadosti_projektove_podpory
+(ziadatel, ico, kod_projektu, nazov_projektu, vuc, cislo_vyzvy,
+    kod_podopatrenia, status, datum_zastavenia_konania,
+    dovod_zastavenie_konania, datum_ucinnosti_zmluvy, schvaleny_nfp_celkom,
+    vyplateny_nfp_celkom, pocet_bodov) VALUES #{placeholders}"
+      conn.exec_params(sql, all_values.flatten)
+      all_values = []
+
+      ap "Inserted #{index} records"
     end
   end
 end
+
+
+
