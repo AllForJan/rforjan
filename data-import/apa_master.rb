ff1ed79236f964ae73f1bf7910483a413cf4f8cd
Ivan Kosdy <ivankosdy@gmail.com>
2018-04-07 14:14:43 +0200

    APA master script update

--- data-import/apa_master.rb
+++ data-import/apa_master.rb
@@ -1,16 +1,44 @@
 require_relative 'dbconn'
+require 'I18n'
+
+I18n.config.available_locales = :en
+
+def normalize_name(name)
+  I18n.transliterate(name).downcase.gsub(/[^\w]/,' ').split.compact.sort.uniq.join(' ')
+end
 
 $conn = db_connection
 
-prijimatelia = $conn.exec('select * from apa_prijimatelia').to_a
-ziadosti = $conn.exec('select * from apa_ziadosti_o_priame_podpory_diely').to_a
+prijimatelia = $conn.exec('select * from apa_prijimatelia limit 10000').to_a
+ziadosti = $conn.exec('select * from apa_ziadosti_o_priame_podpory_diely limit 10000').to_a
 
 
-prijimatelia.each do
-  prijimatelia['meno_norm'] = prijimatelia['meno'].downcase
+prijimatelia.each do |prijimatel|
+  prijimatel['meno_norm'] = normalize_name(prijimatel['meno'])
 end
 
-ziadosti.each do
-  ziadosti['zadiatel_norm'] = ziadosti['ziadatel']
+ziadosti.each do |ziadost|
+  ziadost['ziadatel_norm'] = normalize_name(ziadost['ziadatel'])
 end
+
+master = []
+
+not_found = 0
+duplicate = 0
+
+ziadosti.each do |ziadost|
+  new_row = ziadost
+
+  prijimatel = prijimatelia.select { |p| p['meno_norm'] == ziadost['ziadatel_norm'] }
+  if prijimatel.size > 1
+    puts "Duplicate: #{ziadost.inspect}"
+  elsif prijimatel.size == 1
+    new_row.merge(prijimatel.first)
+  end
+
+  master.append(new_row)
+end
+
+puts "Rows #{master.length}"
+
 
