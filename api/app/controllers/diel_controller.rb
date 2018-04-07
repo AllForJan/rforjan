02bbef061a307058211adb240110915b96176cb2
Martin Scavnicky <martin.scavnicky@gmail.com>
2018-04-08 01:22:09 +0200

    Better structure for diel controller.

--- api/app/controllers/diel_controller.rb
+++ api/app/controllers/diel_controller.rb
@@ -7,18 +7,39 @@ class DielController < ApplicationController
         diel: params[:diel]
     )
 
-    render json: (ziadosti.group_by(&:rok).sort_by(&:first).map do |rok, ziadosti|
+    render json: (ziadosti.group_by(&:rok).sort_by(&:first).map do |rok, ziadosti_rok|
+      ziadatelia = ziadosti_rok.map(&:ziadatel_normalized).uniq
+
       [
           rok,
-          ziadosti.map do |ziadost|
-            ziadost.attributes.merge(
-                celkova_suma: ApaPrijimatelia.celkova_suma(ziadost.ziadatel, rok),
-                celkova_vymera: ApaZiadostiOPriamePodporyDiely.celkova_vymera(ziadost.ziadatel, rok),
-                pocet_dielov: ApaZiadostiOPriamePodporyDiely.pocet_dielov(ziadost.ziadatel, rok, ),
-                pocet_ziadosti: ApaZiadostiOPriamePodporyDiely.pocet_ziadosti(ziadost.ziadatel, rok),
-                vymera_dielu: ziadost.vymera_dielu(rok),
-            )
-          end
+          ziadatelia.map do |ziadatel|
+            ziadosti_ziadatel = ziadosti_rok.select { |z| z.ziadatel_normalized == ziadatel }
+            prijimatelia_ziadatel = ApaPrijimatelia.where(meno_normalized: ziadatel, rok: rok).to_a
+
+            [
+                ziadatel,
+                {
+                  ziadosti: ziadosti_ziadatel.group_by { |z| [z.ziadatel, z.ico] }.map do |key, ziadosti_ico|
+                    {
+                        ziadatel: key[0],
+                        ico: key[1],
+                        pocet_ziadosti: ziadosti_ico.count,
+                        celkova_vymera: ziadosti_ico.sum(&:vymera)
+                    }
+                  end,
+                  prijimatelia: prijimatelia_ziadatel.group_by { |p| [p.meno, p.ico, p.obec, p.psc] }.map do |key, prijimatelia_obec|
+                    {
+                        meno: key[0],
+                        ico: key[1],
+                        obec: key[2],
+                        psc: key[3],
+                        pocet_prijmov: prijimatelia_obec.count,
+                        suma_prijmov: prijimatelia_obec.sum(&:suma)
+                    }
+                  end
+                }
+            ]
+          end.to_h
       ]
     end).to_h
   end
