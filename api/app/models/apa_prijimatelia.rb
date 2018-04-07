6bc8863806537ca9a58d541f1b3c41cec85e790c
Martin Scavnicky <martin.scavnicky@gmail.com>
2018-04-07 17:47:30 +0200

    Add aggregation methods to APA models.

--- api/app/models/apa_prijimatelia.rb
+++ api/app/models/apa_prijimatelia.rb
@@ -1,2 +1,5 @@
 class ApaPrijimatelia < ApplicationRecord
+  def self.celkova_suma(meno, rok)
+      where(meno_normalized: Normalizer.normalize_name(meno), rok: rok).sum(:suma)
+  end
 end
--- api/app/models/apa_ziadosti_o_priame_podpory_diely.rb
+++ api/app/models/apa_ziadosti_o_priame_podpory_diely.rb
@@ -1,3 +1,11 @@
 class ApaZiadostiOPriamePodporyDiely < ApplicationRecord
   self.table_name='apa_ziadosti_o_priame_podpory_diely'
+
+  def self.celkova_vymera(meno, rok)
+    where(ziadatel_normalized: Normalizer.normalize_name(meno), rok: rok).sum(:vymera)
+  end
+
+  def self.pocet_ziadosti(meno, rok)
+    where(ziadatel_normalized: Normalizer.normalize_name(meno), rok: rok).count
+  end
 end
