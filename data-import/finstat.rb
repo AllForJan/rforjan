3f70c2f90ac7e06258e8a86424a11f46d38cc7f6
Branislav Hašto <bhasto@gmail.com>
2018-04-08 08:32:41 +0200

    Finstat importer fix

--- data-import/finstat.rb
+++ data-import/finstat.rb
@@ -6,6 +6,32 @@ require 'tempfile'
 require 'bigdecimal'
 require 'active_support/core_ext/object/blank'
 
+def normalize_csv_row(csv_row)
+  csv_row['Dlhy'] = csv_row['Dlhy'] == 'áno'
+
+  years = 2017.downto(2009)
+  decimal_columns = years.flat_map { |y| ["#{y} Trzby", "#{y} Trzby+vynosy", "#{y} Zisk", "#{y} Aktiva"] }
+  decimal_columns.each do |col|
+    csv_row[col] = BigDecimal(csv_row[col]) if csv_row[col]
+  end
+
+  csv_row.values_at(
+    'Ico', 'Nazov', 'Hlavna cinnost', 'SK Nace', 'Dátum vzniku', 'Dátum zániku',
+    'Dlhy', 'Zamestnanci', 'Zamestnanci - presny pocet',
+    'Adresa', 'Mesto', 'Okres', 'Kraj', 'Štatutári',
+    '2017 Trzby', '2017 Trzby+vynosy', '2017 Zisk', '2017 Aktiva', '2017 Zamestnanci',
+    '2016 Trzby', '2016 Trzby+vynosy', '2016 Zisk', '2016 Aktiva', '2016 Zamestnanci',
+    '2015 Trzby', '2015 Trzby+vynosy', '2015 Zisk', '2015 Aktiva', '2015 Zamestnanci',
+    '2014 Trzby', '2014 Trzby+vynosy', '2014 Zisk', '2014 Aktiva', '2014 Zamestnanci',
+    '2013 Trzby', '2013 Trzby+vynosy', '2013 Zisk', '2013 Aktiva', '2013 Zamestnanci',
+    '2012 Trzby', '2012 Trzby+vynosy', '2012 Zisk', '2012 Aktiva', '2012 Zamestnanci',
+    '2011 Trzby', '2011 Trzby+vynosy', '2011 Zisk', '2011 Aktiva', '2011 Zamestnanci',
+    '2010 Trzby', '2010 Trzby+vynosy', '2010 Zisk', '2010 Aktiva', '2010 Zamestnanci',
+    '2009 Trzby', '2009 Trzby+vynosy', '2009 Zisk', '2009 Aktiva', '2009 Zamestnanci'
+  )
+end
+
+
 Dotenv.load
 
 DB = Sequel.connect(adapter: 'postgres', host: '138.68.66.142', database: 'rforjan', user: 'rforjan', password: ENV['PG_PASS'])
@@ -49,30 +75,4 @@ CSV.foreach('data/prijimatelia_bez_ico_finstat.csv', col_sep: ';', quote_char: '
     imported_count += slice.size
     puts "Imported #{imported_count} rows"
   end
-
-
-def normalize_csv_row(csv_row)
-  csv_row['Dlhy'] = csv_row['Dlhy'] == 'áno'
-
-  years = 2017.downto(2009)
-  decimal_columns = years.flat_map { |y| ["#{y} Trzby", "#{y} Trzby+vynosy", "#{y} Zisk", "#{y} Aktiva"] }
-  decimal_columns.each do |col|
-    csv_row[col] = BigDecimal(csv_row[col]) if csv_row[col]
-  end
-
-  csv_row.values_at(
-    'Ico', 'Nazov', 'Hlavna cinnost', 'SK Nace', 'Dátum vzniku', 'Dátum zániku',
-    'Dlhy', 'Zamestnanci', 'Zamestnanci - presny pocet',
-    'Adresa', 'Mesto', 'Okres', 'Kraj', 'Štatutári',
-    '2017 Trzby', '2017 Trzby+vynosy', '2017 Zisk', '2017 Aktiva', '2017 Zamestnanci',
-    '2016 Trzby', '2016 Trzby+vynosy', '2016 Zisk', '2016 Aktiva', '2016 Zamestnanci',
-    '2015 Trzby', '2015 Trzby+vynosy', '2015 Zisk', '2015 Aktiva', '2015 Zamestnanci',
-    '2014 Trzby', '2014 Trzby+vynosy', '2014 Zisk', '2014 Aktiva', '2014 Zamestnanci',
-    '2013 Trzby', '2013 Trzby+vynosy', '2013 Zisk', '2013 Aktiva', '2013 Zamestnanci',
-    '2012 Trzby', '2012 Trzby+vynosy', '2012 Zisk', '2012 Aktiva', '2012 Zamestnanci',
-    '2011 Trzby', '2011 Trzby+vynosy', '2011 Zisk', '2011 Aktiva', '2011 Zamestnanci',
-    '2010 Trzby', '2010 Trzby+vynosy', '2010 Zisk', '2010 Aktiva', '2010 Zamestnanci',
-    '2009 Trzby', '2009 Trzby+vynosy', '2009 Zisk', '2009 Aktiva', '2009 Zamestnanci'
-  )
-end
 
